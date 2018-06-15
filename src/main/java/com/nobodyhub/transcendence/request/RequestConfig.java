128df57303892a4e7f9dc557ed961736be8e8343
javis-yan <yan_h@hotmail.com>
2018-06-16 00:10:55 +0800

    [transcendence]add RequestConfig

---
+++ src/main/java/com/nobodyhub/transcendence/request/RequestConfig.java
@@ -0,0 +1,13 @@
+package com.nobodyhub.transcendence.request;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author yan_h
+ * @since 2018/6/15
+ */
+@Configuration
+@ComponentScan(basePackages = "com.nobodyhub.transcendence.request")
+public class RequestConfig {
+}
--- src/test/java/com/nobodyhub/transcendence/SpringTest.java
+++ src/test/java/com/nobodyhub/transcendence/SpringTest.java
@@ -1,5 +1,7 @@
 package com.nobodyhub.transcendence;
 
+import com.nobodyhub.transcendence.repository.CassandraConfig;
+import com.nobodyhub.transcendence.request.RequestConfig;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
@@ -10,7 +12,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @since 2018/6/10
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = CassandraConfig.class)
+@ContextConfiguration(classes = {
+        CassandraConfig.class,
+        RequestConfig.class
+})
 @TestPropertySource(locations = "classpath:test.properties")
 public abstract class SpringTest {
 }
