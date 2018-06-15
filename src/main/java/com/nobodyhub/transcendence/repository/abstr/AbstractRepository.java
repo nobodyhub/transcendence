51721d6c7d50e088a2467a9c1265e0596fe5fb98
javis-yan <yan_h@hotmail.com>
2018-06-15 20:37:58 +0800

    [transcendence]make all identifier case-sensitive and add comments for AbstractRepository

--- src/main/java/com/nobodyhub/transcendence/repository/abstr/AbstractRepository.java
+++ src/main/java/com/nobodyhub/transcendence/repository/abstr/AbstractRepository.java
@@ -5,17 +5,22 @@ import com.datastax.driver.core.Session;
 import com.datastax.driver.core.exceptions.InvalidQueryException;
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.nobodyhub.transcendence.model.abstr.Entity;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Repository for basic db operations
  * <b>Note</b>:
  * the column key and column value are both of string type
+ * <p>
+ * identifiers, like table name and column name are quoted with <code>"</code>,
+ * thus they are case-sensitive.
  *
  * @author yan_h
  * @since 2018/6/10
@@ -26,21 +31,57 @@ public abstract class AbstractRepository {
     @Autowired
     protected Cluster cluster;
 
-    protected abstract void createTable(Class<Entity> entityCls);
+    /**
+     * Create table(if not exist) for given entity
+     *
+     * @param entityCls
+     */
+    public abstract void createTable(Class<? extends Entity> entityCls);
 
-    protected abstract void dropTable(Class<Entity> entityCls);
+    /**
+     * Drop table(if exist) for given entity
+     *
+     * @param entityCls
+     */
+    public abstract void dropTable(Class<? extends Entity> entityCls);
 
-    protected abstract void update(Entity entity);
+    /**
+     * Update the content given by entity to related table
+     *
+     * @param entity
+     */
+    public abstract void update(Entity entity);
 
-    protected abstract void query(Entity entity);
+    /**
+     * Query content for given entity
+     * the query entity should have:
+     * 1. non-null value for @Id field
+     * 2. all @Column fields will be fetched
+     * 3. for @ColumnMap field, only fetch those whose keys are contained in map of given entity
+     *
+     * @param entity
+     */
+    public abstract void query(Entity entity);
 
+    /**
+     * Add a set of columns to given table
+     *
+     * @param tableName
+     * @param columnNames
+     */
     protected void addColumns(String tableName, Set<String> columnNames) {
         columnNames.forEach(
                 (columnName) -> addColumn(tableName, columnName)
         );
     }
 
-
+    /**
+     * add column to table
+     * if column exists, will return silently, no exception thrown
+     *
+     * @param tableName
+     * @param columnName
+     */
     protected void addColumn(String tableName, String columnName) {
         try {
             this.session.execute(addColumnCql(tableName, columnName));
@@ -59,8 +100,8 @@ public abstract class AbstractRepository {
      */
     protected String addColumnCql(String table, String column) {
         StringBuilder sb = new StringBuilder();
-        sb.append(String.format(" ALTER TABLE %s ", table));
-        sb.append(String.format(" ADD %s text ", column));
+        sb.append(String.format(" ALTER TABLE \"%s\" ", table));
+        sb.append(String.format(" ADD \"%s\" text ", column));
         return sb.toString();
     }
 
@@ -72,8 +113,8 @@ public abstract class AbstractRepository {
      */
     protected String createTableCql(String table, String idColumnName) {
         StringBuilder sb = new StringBuilder();
-        sb.append(String.format(" CREATE TABLE IF NOT EXISTS %s ( ", table));
-        sb.append(String.format(" %s text PRIMARY KEY ", idColumnName));
+        sb.append(String.format(" CREATE TABLE IF NOT EXISTS \"%s\" ( ", table));
+        sb.append(String.format(" \"%s\" text PRIMARY KEY ", idColumnName));
         sb.append(" ) ");
         return sb.toString();
     }
@@ -85,7 +126,7 @@ public abstract class AbstractRepository {
      * @return
      */
     protected String dropTableCql(String table) {
-        return String.format(" DROP TABLE IF EXISTS %s ", table);
+        return String.format(" DROP TABLE IF EXISTS \"%s\" ", table);
     }
 
     /**
@@ -102,14 +143,20 @@ public abstract class AbstractRepository {
                                String rowKey,
                                Map<String, String> values) {
         StringBuilder sb = new StringBuilder();
-        sb.append(String.format(" UPDATE %s  ", cfName));
+        sb.append(String.format(" UPDATE \"%s\"  ", cfName));
         List<String> assignments = Lists.newArrayList();
         for (Map.Entry<String, String> entry : values.entrySet()) {
-            assignments.add(String.format(" SET %s='%s' ",
-                    entry.getKey(), entry.getValue()));
+            String value = entry.getValue();
+            if (value == null) {
+                assignments.add(String.format(" SET \"%s\"=null ",
+                        entry.getKey()));
+            } else {
+                assignments.add(String.format(" SET \"%s\"='%s' ",
+                        entry.getKey(), value));
+            }
         }
         sb.append(Joiner.on(", ").join(assignments));
-        sb.append(String.format(" WHERE %s='%s'", rowKeyName, rowKey));
+        sb.append(String.format(" WHERE \"%s\"='%s'", rowKeyName, rowKey));
         return sb.toString();
     }
 
@@ -126,10 +173,14 @@ public abstract class AbstractRepository {
                                String rowKeyName,
                                String rowKey,
                                Set<String> columnNames) {
+        Set<String> cols = Sets.newHashSet(columnNames);
+        cols.add(rowKeyName);
         StringBuilder sb = new StringBuilder();
-        sb.append(String.format(" SELECT %s ", Joiner.on(", ").join(columnNames)));
-        sb.append(String.format(" FROM %s ", cfName));
-        sb.append(String.format(" WHERE %s = '%s' ", rowKeyName, rowKey));
+        sb.append(String.format(" SELECT %s ", Joiner.on(", ").join(
+                cols.stream().map(ele -> "\"" + ele + "\"")
+                        .collect(Collectors.toList()))));
+        sb.append(String.format(" FROM \"%s\" ", cfName));
+        sb.append(String.format(" WHERE \"%s\"='%s' ", rowKeyName, rowKey));
         return sb.toString();
     }
 }
